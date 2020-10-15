536b0bb700b2e795b0bb09117f52536efa14d82f
salsabilaysaa <yarasalsabila28912@gmail.com>
2020-10-16 02:47:10 +0700

    first commit

---
+++ app/src/main/java/com/zarra/mytestingapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.zarra.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+
+    private lateinit var imgPreview: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Yara")
+        names.add("Zarra")
+        names.add("Yaya")
+
+        imgPreview = findViewById(R.id.img_preview)
+        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
